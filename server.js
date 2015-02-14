7c251d2e99be843a049b62e9339fd2c4a4fd070c
Adam Back <adb33@email.arizona.edu>
2015-02-13 21:12:51 -0800

    Basic server built.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.set('title', 'Adam Back');
+app.set('port', process.env.PORT || 3000);
+app.set('base url', process.env.URL || 'http://localhost');
+
+var port = app.get('port');
+
 app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.listen(3000);
+app.listen(port);
+console.log('Listening on port ' + port + '.');
