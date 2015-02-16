7906ef125709927b72b7bb3bf482f5996daa7a84
Adam Back <adb33@email.arizona.edu>
2015-02-15 20:52:17 -0800

    Added bootstrap example.

--- server.js
+++ server.js
@@ -10,5 +10,21 @@ var port = app.get('port');
 // GET /style.css etc
 app.use(express.static(__dirname + '/'));
 
+
+// app.get('/bootstrap', function(req, res) {
+//   res.sendFile
+// })
+// app.route('/bootstrap')
+//   .all(function(request, response, next) {
+//     response.sendFile('indexB.html', null, function(error) {
+//       if( error ) {
+//         console.error( 'Error sending indexb.html' );
+//         response.status( error.status ).end();
+//       } else {
+//         console.log( 'Served Bootstrap version.' )
+//       }
+//     });
+//   });
+
 app.listen(port);
 console.log('Listening on port ' + port + '.');
