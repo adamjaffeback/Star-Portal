05f8af8e79a587c95c21f5a7749d5cedf0e25920
Adam Back <adb33@email.arizona.edu>
2017-02-09 23:00:00 -0500

    (cleanup) Swapped all names for new, married name.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('title', 'Adam Back');
+app.set('title', 'Adam Jaffe Back');
 app.set('port', process.env.PORT || 3000);
 app.set('base url', process.env.URL || 'http://localhost');
 
